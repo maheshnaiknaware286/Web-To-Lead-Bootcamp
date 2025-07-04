a96cd70a9332fe24b168faaefac77b555fc72aba
maheshnaiknaware286 <66438894+maheshnaiknaware286@users.noreply.github.com>
2025-07-04 12:29:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+let capthachecked=false;
 function beforesubmit(){
-    let outputdate=document.querySelector(".outputdate");
-    let inputdate=document.querySelector(".inputdate");
-    console.log("inputdate.value",inputdate.value);
-
-    let formattedDate=new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value=formattedDate;
+    if(capthachecked){
+        let outputdate=document.querySelector(".outputdate");
+        let inputdate=document.querySelector(".inputdate");
+        console.log("inputdate.value",inputdate.value);
+    
+        let formattedDate=new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value=formattedDate;
+    }else{
+        alert("Please check the reCAPTCHA box to submit the lead");
+        event.preventDefault();
+    }    
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function capthchasuccess(){
+    capthachecked=true;
+}
